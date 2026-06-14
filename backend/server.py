effe7c484b91ff7770052a361dcdcaa9a0cd1a42
RDS-ENRI <ciaosonoandrea94@gmail.com>
2026-06-14 14:55:09 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -76,7 +76,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=False,
-    allow_methods=["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["Content-Disposition"],
 )
